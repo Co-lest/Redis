5eb15d174e3f6576524c222e0a4eb7c62380a4fb
Mark Tom <122617440+Co-lest@users.noreply.github.com>
2024-11-05 20:52:25 +0300

    SETGET

--- app/main.js
+++ app/main.js
@@ -10,15 +10,15 @@ const server = net.createServer((connection) => {
     // *2\r\n $5 \r\n ECHO \r\n $3 \r\n hey \r\n
     const commands = Buffer.from(data).toString().split("\r\n");
  
-    if (commands[2] == "ECHO") { //command["*2", "$5", "ECHO", "$3", "hey"]
-      const stringEcho = commands[4];
+    if (commands[2] == "ECHO") { //command["*2", "$5", "ECHO", "$3", "hey", "\r\n"]
+      const stringEcho = commands[commands.length - 2];
       const len = stringEcho.length;
       return connection.write("$" + len + "\r\n" + stringEcho + "\r\n");
     } else if (commands[2] == "SET") {//command["*2", "$5", "SET", "$3", "hey"]
         return connection.write("+OK\r\n")
-    } else if(commands[2] == "GET") {
-        const len2 = commands[5].length
-        return connection.write("$" + len2 + "\r\n" + commands[5] + "\r\n")
+    } else if(commands[2] == "GET") {  //command["*3", "\r\n",  "SET", "\r\n" "$3", "\r\n", "foo", "\r\n", "$3", "\r\n", "bar", "\r\n"]
+        const len2 = commands[10].length
+        return connection.write("$" + len2 + "\r\n" + commands[10] + "\r\n")
     }
 
     connection.write("+PONG\r\n")
@@ -26,3 +26,6 @@ const server = net.createServer((connection) => {
 });
 
 server.listen(6379, "127.0.0.1");
+
+
+//*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n
