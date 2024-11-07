0f68808066b5941a8a6d8493e14910e224ba5943
Mark Tom <122617440+Co-lest@users.noreply.github.com>
2024-11-07 22:37:35 +0300

    PONG

--- app/main.js
+++ app/main.js
@@ -5,45 +5,20 @@ console.log("Logs from your program will appear here!");
 
 //Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
-  connection.on("data", (data) => {
-    // *2\r\n $5 \r\n ECHO \r\n $3 \r\n hey \r\n
-    const commands = Buffer.from(data).toString().split("\r\n");
- 
-    if (commands[2] == "ECHO") { //command["*2", "$5", "ECHO", "$3", "hey", "\r\n"]
-      const stringEcho = commands[4];
-      const len = stringEcho.length;
-      return connection.write("$" + len + "\r\n" + stringEcho + "\r\n");
-    }
+  console.log("Data received:" + data.toString());
+  const msg = data.toString().split("\r\n");
+  const command = msg[2];
+  const arg = msg[4];
 
-    const command = commands[4]; // set or get
-
-    if (command === "SET") {
-        const key = commands[8];
-        const value = commands[12];
-        
-        map.set(key, value); 
-
-        connection.write("+OK\r\n");
-
-    } else if (command === "GET") {
-        const key = commands[8];
-        
-        if (!key || !map.has(key)) {
-            connection.write("$-1\r\n");
-        }
-
-        const value = map.get(key);
-
-        connection.write(`$${value.length}\r\n${value}\r\n`);
-    }
-
-    connection.write("+PONG\r\n")
-  })
+  switch(command) {
+    case "PING":
+      connection.write(`"+PONG\r\n"`)
+      break
+  }
 });
 
 server.listen(6379, "127.0.0.1");
 
-
+// have tried so much
 // *3 \r\n $3 \r\n SET \r\n $3 \r\n foo \r\n $3 \r\n bar \r\n
 // *3 \r\n $3 \r\n GET \r\n $3 \r\n foo \r\n
