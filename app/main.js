c3cba2f0ae112ddf88d46957d7098da2b096f973
Mark Tom <122617440+Co-lest@users.noreply.github.com>
2024-11-08 11:37:07 +0300

    directory

---
+++ app/main.js
@@ -0,0 +1,42 @@
+const net = require("net");
+
+const buff = {};
+let duration;
+
+console.log("Logs from your program will appear here!");
+
+const server = net.createServer((connection) => {
+  connection.on("data", (data) => {
+    // *2\r\n $5 \r\n ECHO \r\n $3 \r\n hey \r\n
+    const commands = Buffer.from(data).toString().split("\r\n");
+    //command["*2", "$5", "ECHO", "$3", "hey"]
+
+    if (commands[2] == "ECHO") {
+      const stringEcho = commands[4];
+      const len = stringEcho.length;
+      return connection.write("$" + len + "\r\n" + stringEcho + "\r\n");
+    } else if (commands[2] == "SET") {
+      buff[commands[4]] = commands[6];
+      if ((commands[10])) { //(commands[8]).toLocaleLowerCase() == "pt"
+        duration = commands[10]
+        setTimeout(() => {
+          delete buff[commands[4]]
+        }, duration);
+      }
+      return connection.write("+OK\r\n");
+    } else if (commands[2] == "GET") {
+      if (buff[commands[4]]) {
+        return connection.write(`$${(buff[commands[4]]).length}\r\n${buff[commands[4]]}\r\n`)
+      } else {
+        return connection.write(`$-1\r\n`);
+      }
+    } else if (commands[2] == "--dir") { //*2 \r\n $3 \r\n dir \r\n $16 \r\n/ tmp/redis-files \r\n
+      return connection.write(`$${(commands[4]).length}\r\n${commands[4]}\r\n$${(commands[8]).length}\r\n${commands[length]}\r\n`);
+    }
+    connection.write(`+PONG\r\n`);
+  });
+});
+server.listen(6379, "127.0.0.1");
+
+
+//*2 $n --dir $n /tmp/redis-files $n --dbfilename $n dump.rdb
