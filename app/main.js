de4e0e7d631be69c17722026b0e22536694a946c
Mark Tom <122617440+Co-lest@users.noreply.github.com>
2024-11-12 20:30:04 +0300

    directory

--- app/main.js
+++ app/main.js
@@ -1,61 +1,55 @@
 const net = require("net");
 
-const buff = {};
-let duration;
-let dataStore = new Map()
-
+let dataStore = new Map();
 console.log("Logs from your program will appear here!");
+
+const parseCommand = (data) => {
+  const inputArray = data.toString().split('\r\n');
+  const command = inputArray[2]?.toLowerCase();
+  const key = inputArray[4] || '';
+  const value = inputArray[6] || '';
+  const option = inputArray[8]?.toLowerCase();
+  const optionValue = inputArray[10] || '';
+  return { command, key, value, option, optionValue };
+};
 
 const server = net.createServer((connection) => {
   connection.on("data", (data) => {
-    // *2\r\n $5 \r\n ECHO \r\n $3 \r\n hey \r\n
-    const commands = Buffer.from(data).toString().split("\r\n");
-    const inputString = data.toString();
-		const inputArray = inputString.split('\r\n');
-
-    const [,, dir, path, dbfilename, file] = process.argv;
-
-    const [, , command, , key = '', , value = '',,key2='',, value2=''] = inputArray;
-		const cmd = command.toLowerCase();
-		const cmd2 = value2.toLowerCase();
-
-    if (key2 === cmd2) {
-			dataStore.set(cmd2, value2)
-		  }
-		
-
-    if (commands[2] == "ECHO") {
-      const stringEcho = commands[4];
-      const len = stringEcho.length;
-      return connection.write("$" + len + "\r\n" + stringEcho + "\r\n");
-    } else if (commands[2] == "SET") {
-      buff[commands[4]] = commands[6];
-      if ((commands[10])) { //(commands[8]).toLocaleLowerCase() == "pt"
-        duration = commands[10]
-        setTimeout(() => {
-          delete buff[commands[4]]
-        }, duration);
+    const { command, key, value, option, optionValue } = parseCommand(data);
+ 
+      if (command === "echo") {
+        const len = value.length;
+        return connection.write(`$${len}\r\n${value}\r\n`);
       }
-      return connection.write("+OK\r\n");
-    } else if (commands[2] == "GET") {
-      if (buff[commands[4]]) {
-        return connection.write(`$${(buff[commands[4]]).length}\r\n${buff[commands[4]]}\r\n`)
-      } else {
-        return connection.write(`$-1\r\n`);
+
+      if (command === "set") {
+        dataStore.set(key, value);
+        if (option === "px") {
+          const duration = parseInt(optionValue, 10);
+          setTimeout(() => dataStore.delete(key), duration);
+        }
+        return connection.write("+OK\r\n");
       }
-    } else if (cmd == "config") { //*2 \r\n $3 \r\n dir \r\n $16 \r\n/ tmp/redis-files \r\n
-      dataStore.set('dir', path);
-      dataStore.set('dbfilename', file);
-      let result = dataStore.get(value);
-      console.log(result);
-      const responseArr = [`$${value.length}\r\n${value}\r\n`, `$${result.length}\r\n${result}\r\n`];
-      const redisResponse = `*${responseArr.length}\r\n${responseArr.join('')}`;
-      return connection.write(redisResponse);
-    }
-    connection.write(`+PONG\r\n`);
+
+      if (command === "get") {
+        const storedValue = dataStore.get(key);
+        if (storedValue) {
+          return connection.write(`$${storedValue.length}\r\n${storedValue}\r\n`);
+        } else {
+          return connection.write("$-1\r\n");
+        }
+      }
+
+      if (command === "config") {
+        const [, , dir, path, dbfilename, file] = process.argv;
+        dataStore.set('dir', path);
+        dataStore.set('dbfilename', file);
+        const configValue = dataStore.get(value) || '';
+        const response = `*2\r\n$${value.length}\r\n${value}\r\n$${configValue.length}\r\n${configValue}\r\n`;
+        return connection.write(response);
+      }
+        connection.write("+PONG\r\n");
   });
 });
+
 server.listen(6379, "127.0.0.1");
-
-
-//*2 $n --dir $n /tmp/redis-files $n --dbfilename $n dump.rdb
